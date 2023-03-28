587ecb0ca9b9daea8dd145620dfa6495243da7d3
Oscar Galvez <gcoscar99@yahoo.com>
2023-03-28 16:03:22 -0700

    bug en el carrito de compras resuelto

--- src/providers/MenuProvider.jsx
+++ src/providers/MenuProvider.jsx
@@ -1,14 +1,14 @@
 import { createContext, useState, useEffect } from 'react'
 import { toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { categorias as categoriasDB } from '../data/categorias';
+import clienteAxios from '../config/axios';
 
 const MenuContext = createContext();
 
 const MenuProvider = ({children}) => {
 
-    const [categorias, setCategorias] = useState(categoriasDB);
-    const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
+    const [categorias, setCategorias] = useState([]);
+    const [categoriaActual, setCategoriaActual] = useState({});
     const [modal, setModal] = useState(false);
     const [producto, setProducto] = useState({});
     const [pedido, setPedido] = useState([]);
@@ -18,6 +18,20 @@ const MenuProvider = ({children}) => {
         const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
         setTotal(nuevoTotal)
     }, [pedido])
+
+    const obtenerCategorias = async () => {
+        try {
+            const {data} = await clienteAxios('/api/categorias')
+            setCategorias(data.data)
+            setCategoriaActual(data.data[0])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        obtenerCategorias();
+    },[])
 
     const handleClickCategoria = id =>{
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
